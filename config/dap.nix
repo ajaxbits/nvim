b7bb57c592f9c7e64b40267efb7ec9d2a388620f
Alex Jackson <contact@ajaxbits.com>
2024-03-02 16:11:16 -0600

    feat: add debugging keybinds
    test

--- config/dap.nix
+++ config/dap.nix
@@ -7,4 +7,72 @@
       dap-virtual-text.enable = true;
     };
   };
+
+  extraConfigLua = ''
+    require("dap").listeners.before.attach.dapui_config = function()
+      require("dapui").open()
+    end
+    require("dap").listeners.before.launch.dapui_config = function()
+      require("dapui").open()
+    end
+    require("dap").listeners.before.event_terminated.dapui_config = function()
+      require("dapui").close()
+    end
+    require("dap").listeners.before.event_exited.dapui_config = function()
+      require("dapui").close()
+    end
+  '';
+
+  keymaps = [
+    {
+      key = "<leader>dd";
+      action = "<cmd> DapContinue<CR>";
+      options.silent = true;
+      mode = ["n"];
+    }
+    {
+      key = "<leader>ds";
+      action = "<cmd> DapTerminate<CR>";
+      options.silent = true;
+      mode = ["n"];
+    }
+    {
+      key = "<leader>di";
+      action = "<cmd> DapStepInto<CR>";
+      options.silent = true;
+      mode = ["n"];
+    }
+    {
+      key = "<leader>do";
+      action = "<cmd> DapStepOut<CR>";
+      options.silent = true;
+      mode = ["n"];
+    }
+    {
+      key = "<leader>dt";
+      action = "require('dapui').toggle";
+      lua = true;
+      options.silent = true;
+      mode = ["n"];
+    }
+    {
+      key = "<leader>dl";
+      action = "<cmd> DapLoadLaunchJSON<CR>";
+      options.silent = true;
+      mode = ["n"];
+    }
+    {
+      key = "<leader>db";
+      action = "<cmd> DapToggleBreakpoint<CR>";
+      options.silent = true;
+      mode = ["n" "v"];
+    }
+    {
+      key = "<C-k>";
+      action = "require('dapui').eval";
+      lua = true;
+      options.silent = true;
+      mode = ["v"];
+    }
+  ];
 }
