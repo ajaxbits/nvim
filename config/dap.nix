3eb975fa432e3f84b6ada1ea488987e2b9bbcf6c
Alex Jackson <contact@ajaxbits.com>
2024-04-15 13:12:33 -0500

    feat: don't close dap automatically on debug session completion

--- config/dap.nix
+++ config/dap.nix
@@ -18,12 +18,6 @@
     require("dap").listeners.before.launch.dapui_config = function()
       require("dapui").open()
     end
-    require("dap").listeners.before.event_terminated.dapui_config = function()
-      require("dapui").close()
-    end
-    require("dap").listeners.before.event_exited.dapui_config = function()
-      require("dapui").close()
-    end
   '';
 
   keymaps = [
