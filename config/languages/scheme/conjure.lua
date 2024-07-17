6355459845a7b80ffb2c441d918ed087adfaa359
Alex Jackson <contact@ajaxbits.com>
2024-07-17 18:48:20 -0500

    fix: restrict conjure to only schemes

--- config/languages/scheme/conjure.lua
+++ config/languages/scheme/conjure.lua
@@ -2,3 +2,4 @@ vim.g["conjure#client#scheme#stdio#command"]              = "@csi@ -quiet -:c"
 vim.g["conjure#client#scheme#stdio#prompt_pattern"]       = "\n-#;%d-> "
 vim.g["conjure#client#scheme#stdio#value_prefix_pattern"] = false
 vim.g["conjure#mapping#prefix"]                           = "<leader>"
+vim.g["conjure#filetypes"]                                = { "scheme", "fennel", "clojure" }
