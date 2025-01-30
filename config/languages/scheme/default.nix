47c026f23d3dcdb5527ee881622368a9a74a2f29
Alex Jackson <contact@ajaxbits.com>
2025-01-30 13:31:53 -0500

    chore: disable conjure to improve startup time

--- config/languages/scheme/default.nix
+++ config/languages/scheme/default.nix
@@ -1,16 +1,17 @@
 { pkgs, lib, ... }:
 {
+  # TODO: revisit once lazy loading is a thing
   plugins = {
-    conjure.enable = true;
-    parinfer-rust.enable = true;
+    conjure.enable = false;
+    parinfer-rust.enable = false;
   };
-
-  extraConfigLua = builtins.readFile (
-    pkgs.substituteAll {
-      name = "conjure.lua";
-      src = ./conjure.lua;
-
-      csi = lib.getExe' pkgs.chicken "chicken";
-    }
-  );
+  #
+  # extraConfigLua = builtins.readFile (
+  #   pkgs.substituteAll {
+  #     name = "conjure.lua";
+  #     src = ./conjure.lua;
+  #
+  #     csi = lib.getExe' pkgs.chicken "chicken";
+  #   }
+  # );
 }
