12c9567368978a6408a5957e00c3072b7c4d3449
Alex Jackson <contact@ajaxbits.com>
2025-06-02 13:49:03 -0500

    fix: add terraform conform

--- config/languages/terraform/default.nix
+++ config/languages/terraform/default.nix
@@ -1,7 +1,12 @@
+{ pkgs, lib, ... }:
 {
   plugins.lsp.servers = {
     tflint.enable = true;
     terraformls.enable = true;
+  };
+  plugins.conform-nvim.settings = {
+    formatters_by_ft.terraform = [ "terraform_fmt" ];
+    formatters.terraform_fmt.command = lib.getExe pkgs.opentofu;
   };
   extraConfigLua = builtins.readFile ./default.lua;
 }
