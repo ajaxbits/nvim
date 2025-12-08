cfb43db8af874e802472b7aabbb308d64ac7b706
Alex Jackson <contact@ajaxbits.com>
2025-12-08 11:12:14 -0600

    fix: fix leap bindings

--- config/default.nix
+++ config/default.nix
@@ -103,6 +103,22 @@
         options.silent = true;
         mode = [ "n" ];
       }
+      {
+        action = "<Plug>(leap)";
+        key = "s";
+        options.silent = true;
+        mode = [
+          "n"
+          "x"
+          "o"
+        ];
+      }
+      {
+        action = "<Plug>(leap-from-window)";
+        key = "S";
+        options.silent = true;
+        mode = [ "n" ];
+      }
     ];
 
     autoCmd = [
@@ -143,7 +159,36 @@
           '';
         write_all_buffers = true;
       };
-      leap.enable = true;
+      leap = {
+        enable = true;
+        luaConfig.post = # lua
+          ''
+            -- Highly recommended: define a preview filter to reduce visual noise
+            -- and the blinking effect after the first keypress
+            -- (`:h leap.opts.preview`). You can still target any visible
+            -- positions if needed, but you can define what is considered an
+            -- exceptional case.
+            -- Exclude whitespace and the middle of alphabetic words from preview:
+            --   foobar[baaz] = quux
+            --   ^----^^^--^^-^-^--^
+            require('leap').opts.preview = function (ch0, ch1, ch2)
+              return not (
+                ch1:match('%s')
+                or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+              )
+            end
+
+            -- Define equivalence classes for brackets and quotes, in addition to
+            -- the default whitespace group:
+            require('leap').opts.equivalence_classes = {
+              ' \t\r\n', '([{', ')]}', '\'"`'
+            }
+
+            -- Use the traversal keys to repeat the previous motion without
+            -- explicitly invoking Leap:
+            require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+          '';
+      };
       rainbow-delimiters.enable = true;
       faster.enable = true;
       nvim-autopairs = {
