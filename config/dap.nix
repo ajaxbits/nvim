192ed700379699acfa2241f39f684578ddb10556
Alex Jackson <contact@ajaxbits.com>
2025-12-11 21:53:40 -0600

    feat: mess with dap to further speed up performance

--- config/dap.nix
+++ config/dap.nix
@@ -1,11 +1,28 @@
-{ pkgs, lib, ... }:
 {
   plugins = {
-    dap.enable = true;
+    # TODO: it looks like the dap API changed. None of my commands work anymore. gotta use lua. Look into lazy loading while you're at it.
+    dap = {
+      enable = true;
+      luaConfig.pre =
+        # Lua
+        ''
+          require("dap").listeners.before.attach.dapui_config = function()
+            require("dapui").open()
+          end
+          require("dap").listeners.before.launch.dapui_config = function()
+            require("dapui").open()
+          end
+          require("dap").listeners.before.event_terminated.dapui_config = function()
+            require("dapui").close()
+          end
+          require("dap").listeners.before.event_exited.dapui_config = function()
+            require("dapui").close()
+          end
+        '';
+    };
     dap-python = {
       enable = true;
       testRunner = "pytest";
-      lazyLoad.settings.ft = [ "python" ];
     };
     dap-ui = {
       enable = true;
@@ -17,32 +34,47 @@
           "DapStepInto"
           "DapStepOut"
         ];
+        before.__raw = ''
+          function()
+            require('lz.n').trigger_load('nvim-dap', {})
+            require('lz.n').trigger_load('nvim-dap-virtual-text', {})
+            require("dap").listeners.before.attach.dapui_config = function()
+              require("dapui").open()
+            end
+            require("dap").listeners.before.launch.dapui_config = function()
+              require("dapui").open()
+            end
+          end
+        '';
+        keys = [
+          {
+            __unkeyed-1 = "<leader>dt";
+            __unkeyed-2.__raw = ''
+              function()
+                require("dapui").toggle()
+              end
+            '';
+            desc = "Toggle Debugger UI";
+          }
+        ];
       };
     };
-    dap-virtual-text.enable = true;
+    dap-virtual-text = {
+      enable = true;
+      lazyLoad.settings = {
+        before.__raw = ''
+          function()
+            require('lz.n').trigger_load('nvim-dap')
+          end
+        '';
+        cmd = [
+          "DapVirtualTextEnable"
+          "DapVirtualTextForceRefresh"
+          "DapVirtualTextToggle"
+        ];
+      };
+    };
   };
-
-  # extraPlugins = with pkgs.vimUtils; [
-  #   (buildVimPlugin {
-  #     # name = "vim-dap-vscode-js";
-  #     name = "nvim-dap-vscode-js";
-  #     src = pkgs.fetchFromGitHub {
-  #       owner = "mxsdev";
-  #       repo = "nvim-dap-vscode-js";
-  #       rev = "03bd29672d7fab5e515fc8469b7d07cc5994bbf6";
-  #       hash = "sha256-8dlzaI94I8ppzLRNIfjVd92kYgqr07D1akDj4VdKQto=";
-  #     };
-  #   })
-  # ];
-
-  extraConfigLua = ''
-    require("dap").listeners.before.attach.dapui_config = function()
-      require("dapui").open()
-    end
-    require("dap").listeners.before.launch.dapui_config = function()
-      require("dapui").open()
-    end
-  '';
 
   keymaps = [
     {
@@ -70,12 +102,6 @@
       mode = [ "n" ];
     }
     {
-      key = "<leader>dt";
-      action.__raw = "require('dapui').toggle";
-      options.silent = true;
-      mode = [ "n" ];
-    }
-    {
       key = "<leader>dl";
       action = "<cmd> DapLoadLaunchJSON<CR>";
       options.silent = true;
@@ -89,12 +115,6 @@
         "n"
         "v"
       ];
-    }
-    {
-      key = "<C-k>";
-      action.__raw = "require('dapui').eval";
-      options.silent = true;
-      mode = [ "v" ];
     }
   ];
 }
