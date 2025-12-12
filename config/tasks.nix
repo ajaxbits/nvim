eebc2c25bddb1bf0192222c8e9dee0acaa88f69e
Alex Jackson <contact@ajaxbits.com>
2025-12-12 09:13:59 -0600

    chore: refine Overseer

--- config/tasks.nix
+++ config/tasks.nix
@@ -2,20 +2,20 @@
   plugins = {
     overseer = {
       enable = true;
-      lazyLoad.settings.cmd = [
-        "OverseerOpen"
-        "OverseerClose"
-        "OverseerToggle"
-        "OverseerSaveBundle"
-        "OverseerLoadBundle"
-        "OverseerDeleteBundle"
-        "OverseerRunCmd"
-        "OverseerRun"
-        "OverseerInfo"
-        "OverseerBuild"
-        "OverseerQuickAction"
-        "OverseerTaskAction"
-        "OverseerClearCache"
+      lazyLoad.settings.before.__raw = ''
+        function()
+          require "lz.n".trigger_load "snacks.nvim"
+        end
+      '';
+      lazyLoad.settings.keys = [
+        {
+          __unkeyed-1 = "<leader>ro";
+          __unkeyed-2 = "<cmd>OverseerToggle<cr>";
+        }
+        {
+          __unkeyed-1 = "<leader>rr";
+          __unkeyed-2 = "<cmd>OverseerRun<cr>";
+        }
       ];
     };
   };
