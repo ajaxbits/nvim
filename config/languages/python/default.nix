96cd3b3380ceb04550f55fbe4edc3c6533583a56
Alex Jackson <contact@ajaxbits.com>
2025-12-11 22:29:23 -0600

    feat: migrate to official venv-selector plugin

--- config/languages/python/default.nix
+++ config/languages/python/default.nix
@@ -1,7 +1,13 @@
-{ pkgs, ... }:
 {
   config = {
     plugins = {
+      venv-selector = {
+        enable = true;
+        lazyLoad.settings.ft = [ "python" ];
+        settings = {
+          dap_enabled = true;
+        };
+      };
       lsp = {
         enable = true;
         servers = {
@@ -15,19 +21,5 @@
         };
       };
     };
-
-    extraConfigLua = builtins.readFile ./venv-selector.lua;
-    extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        name = "venv-selector.nvim";
-        src = pkgs.fetchFromGitHub {
-          owner = "linux-cultist";
-          repo = "venv-selector.nvim";
-          rev = "e82594274bf7b54387f9a2abe65f74909ac66e97";
-          hash = "sha256-AyxITaKoeM+l+RbFp2UWy0zVrxrIxy8S/oDJsEr/VDQ=";
-        };
-        doCheck = false; # Tests are broken for some reason
-      })
-    ];
   };
 }
